8f292509f99648f15b400e36574349a3a2998676
devfailure <tbanks@US-TYLERBA-7480.us.trendnet.org>
2019-11-14 17:07:03 -0600

    Auth Request

--- node/getCloudAccountsMonitoring/index.js
+++ node/getCloudAccountsMonitoring/index.js
@@ -1,12 +1,20 @@
 const request = require('request-promise');
+require('dotenv').config()
 const host = 'https://app.deepsecurity.trendmicro.com';
 
 
 request({
-    "method": "POST",
-    "uri": host + '/rest/authentication/login',
-    "json": true,
-    "headers": {
-        "User-Agent": ""
+    'method': 'POST',
+    'uri': host + '/rest/authentication/login',
+    'json': true,
+    'body': {
+        dsCredentials: {
+          userName: process.env.USERNAME,
+          password: process.env.PASSWORD,
+          tenantName: process.env.TENANT
+        }
+      },
+    'headers': {
+        'Content-Type': 'application/json'
     }
 }).then(console.log, console.log);
